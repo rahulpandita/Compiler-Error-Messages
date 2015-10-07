0514539d9f4237b1456be2429c94369051637e5c
Rahul Pandita <rahulpandita@gmail.com>
2015-10-07 08:52:13 -0400

    Refactoring

--- src/src/main/java/edu/ncsu/csc/dlf/CompilerErrorStudy/App.java
+++ src/src/main/java/edu/ncsu/csc/dlf/CompilerErrorStudy/App.java
@@ -1,5 +1,7 @@
 package edu.ncsu.csc.dlf.CompilerErrorStudy;
 
+import edu.ncsu.csc.dlf.javaSrcCrawler.JavaSourceCrawler;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	JavaSourceCrawler crawler = new JavaSourceCrawler();
+    	crawler.crawl();
     }
 }
